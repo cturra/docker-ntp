683ec3989d2b1c289108be44491bce498f90b22c
Andreas Faerber <af@maeh.org>
2020-04-13 10:05:38 +0200

    Added travis integration and multiarch builds to push to docker hub

---
+++ ci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
+
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  docker buildx build \
+    --progress plain \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    -t $DOCKER_REPO:$TRAVIS_BRANCH \
+    --push \
+    .
+  exit $?
+fi
+
+TAG="${TRAVIS_TAG:-latest}"
+echo "TAG: $TAG"
+docker buildx build \
+     --progress plain \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    -t $DOCKER_REPO:$TAG \
+    --push \
+    .
+
