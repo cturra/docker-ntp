a5c9f1a04e8fc1e5b8007e71b107ffb2dc92f916
cturra <cturra@gmail.com>
2023-01-26 12:16:24 -0800

    adding noclientlog chronyd option and minor cleanup
    as requested by @Gontier-Julien, i have added the ability to set
    the noclientlog config option via an environment variable.
    
    additionally, the rtcsync option is being removed since this
    container has no access to the system clock so it's not needed.
    
    finally, i addressed a small bug in the local timeserver check.
    it previously would have matches on any IP address that contained
    127 in the 1-3 octets. which is not what we want... it needs to
    match 127.0/8

--- assets/startup.sh
+++ assets/startup.sh
@@ -51,7 +51,7 @@ for N in $NTP_SERVERS; do
 
   # check if ntp server has a 127.0.0.0/8 address (RFC3330) indicating it's
   # the local system clock
-  if [[ "${N_CLEANED}" == *"127\."* ]]; then
+  if [[ "${N_CLEANED}" == "127\."* ]]; then
     echo "server "${N_CLEANED} >> ${CHRONY_CONF_FILE}
     echo "local stratum 10"    >> ${CHRONY_CONF_FILE}
 
@@ -66,7 +66,9 @@ done
   echo
   echo "driftfile /var/lib/chrony/chrony.drift"
   echo "makestep 0.1 3"
-  echo "rtcsync"
+  if [ "${NOCLIENTLOG}" = true ]; then
+    echo "noclientlog"
+  fi
   echo
   echo "allow all"
 } >> ${CHRONY_CONF_FILE}
